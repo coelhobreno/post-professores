ae56b881a11eeb0b1667cbd7667d860678488f2e
Breno Coelho <coelhobreno21@gmail.com>
2023-09-22 11:30:59 -0300

    retirnado config firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPnSrxZ3q0nJL029sOzGTrok73O9WJiq8",
-  authDomain: "posts-professores.firebaseapp.com",
-  projectId: "posts-professores",
-  storageBucket: "posts-professores.appspot.com",
-  messagingSenderId: "1019331900946",
-  appId: "1:1019331900946:web:34c9bab2143d206f5b8acb"
+  /*config firebase*/
 };
 
 const app = initializeApp(firebaseConfig);
