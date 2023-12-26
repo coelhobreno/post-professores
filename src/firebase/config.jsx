b8845724922c77d126789948222d78874c43aa05
Breno Coelho <coelhobreno21@gmail.com>
2023-12-26 04:50:42 -0300

    organizing firebase

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,14 +2,9 @@ import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBPnSrxZ3q0nJL029sOzGTrok73O9WJiq8",
-  authDomain: "posts-professores.firebaseapp.com",
-  projectId: "posts-professores",
-  storageBucket: "posts-professores.appspot.com",
-  messagingSenderId: "1019331900946",
-  appId: "1:1019331900946:web:34c9bab2143d206f5b8acb"
-};
+import firebaseConfig from "./firebaseConfig";
+
+import 'dotenv/config'
 
 const app = initializeApp(firebaseConfig);
 
